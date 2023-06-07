da0581a77f1227d1b2bfa22d3871612a205d15db
Tick <76979689+dgrtygry@users.noreply.github.com>
2023-06-07 14:13:20 -0500

    Update gaa.bat

--- chatbot-brain/gaa.bat
+++ chatbot-brain/gaa.bat
@@ -1,37 +1,41 @@
 @echo off
 
+REM Define file paths
+set "data_file=data.txt"
+set "brain_file=brain.txt"
+
 REM Create data.txt if it doesn't exist
-if not exist data.txt (
+if not exist "%data_file%" (
     echo Creating data.txt...
-    echo Initial data.>data.txt
+    echo Initial data.>"%data_file%"
     echo.
 )
 
 REM Create brain.txt if it doesn't exist
-if not exist brain.txt (
+if not exist "%brain_file%" (
     echo Creating brain.txt...
-    echo Initial brain state.>brain.txt
+    echo Initial brain state.>"%brain_file%"
     echo.
 )
 
 REM Display current brain state
-echo Current brain state (brain.txt):
-type brain.txt
+echo Current brain state (%brain_file%):
+type "%brain_file%"
 echo.
 
 REM Prompt user for input
 set /p input=Enter your message: 
 
 REM Append input to data.txt
-echo %input%>>data.txt
+echo %input%>>"%data_file%"
 
 REM Call training.bat to process the data
-echo Calling training.bat to update brain.txt...
-call training.bat data.txt brain.txt
+echo Calling training.bat to update %brain_file%...
+call training.bat "%data_file%" "%brain_file%"
 
 REM Display updated brain state
-echo Updated brain state (brain.txt):
-type brain.txt
+echo Updated brain state (%brain_file%):
+type "%brain_file%"
 echo.
 
 REM End of gaa.bat
