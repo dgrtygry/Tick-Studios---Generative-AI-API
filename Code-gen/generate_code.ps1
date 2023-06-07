1e10f38b140f1416a429b4e1f7dc279bc3ec1769
Tick <76979689+dgrtygry@users.noreply.github.com>
2023-06-07 14:55:55 -0500

    Create generate_code.ps1

---
+++ Code-gen/generate_code.ps1
@@ -0,0 +1,14 @@
+# Read user input from the PowerShell window
+$codeInput = Read-Host "Enter code input: "
+
+# Read code brain from "codebrain.txt" file
+$codeBrain = Get-Content -Path ".\codebrain.txt" -Raw
+
+# Read code data from "codedata.txt" file
+$codeData = Get-Content -Path ".\codedata.txt" -Raw
+
+# Generate unique code based on user input
+$generatedCode = $codeBrain + $codeInput + $codeData
+
+# Save the generated code as "code.bin"
+$generatedCode | Set-Content -Path ".\code.bin" -Encoding Byte
